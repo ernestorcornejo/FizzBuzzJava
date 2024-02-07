f65f625237126c284ac1581531556c01ed95f9c5
ernestorcornejo <147128656+ernestorcornejo@users.noreply.github.com>
2024-02-06 20:22:29 -0800

    Updating a few things
    Getting fancy wid it

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+        Main b = new Main();
+        b.Dog();//Calling th func
         System.out.println("Hello, World!");
         System.out.println("Y"); //joel is gay
+    }
+
+    public void Dog()
+    {
+        System.out.println("WOOF");
     }
 }
