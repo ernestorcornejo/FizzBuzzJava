79e82f1c48ef98b3add7336bb91fd9e0939e76ad
ernestorcornejo <147128656+ernestorcornejo@users.noreply.github.com>
2024-02-06 21:29:48 -0800

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,26 @@
-public class FizzBuzz {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Y"); //joel is gay
+public class FizzBuzz 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        int i = 1;
+        while (i < 101) 
+        {
+            if(i % 15 == 0)
+            {//f
+                System.out.println("FizzBuzz"+ ":  "+ i);
+            }
+            else if (i % 3 == 0)
+            {//T
+                System.out.println("Fizz" + ":  "+ i);
+            }
+            else if (i % 5 == 0) 
+            {//F
+                System.out.println("Buzz"+ ":  "+ i);
+            }
+            i++;//INCREMENTOR NEEDS TO BE INSIDE OF THE WHILE LOOP!!!!!!!!!!!!
+        }
+        
     }
 }
+            
+
