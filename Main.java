96f74ef6521d0e52806331d2b7c8422c37c1fc6a
ernestorcornejo <147128656+ernestorcornejo@users.noreply.github.com>
2024-02-06 20:05:35 -0800

    Update Main.java
    adding a comment

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Y");
+        System.out.println("Y"); //joel is gay
     }
 }
